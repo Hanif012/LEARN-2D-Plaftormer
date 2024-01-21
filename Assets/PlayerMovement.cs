8eacbdfa976d7535da06a72d3a9b569e16700137
GeezNuts <georgesuitela05@gmail.com>
2024-01-21 23:21:09 +0700

    ragdoll trial

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,20 +9,21 @@ public class PlayerController : MonoBehaviour
 
     public BoxCollider2D playerCollider;
     public Animator animator;
-    
+    public bool ragdoll = false;
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 10f;
 
     public bool isJumpPressed = false;
     public bool facingRight = true;
     public Transform transform;
-
+    public GameObject childObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<BoxCollider2D>();
+        // CapsuleCollider2D childCollider = childObject.GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -51,6 +52,17 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("grounded",false);
 
         animator.SetFloat("yaxis",rb.velocity.y );
+        if(Input.GetKeyDown(KeyCode.Z)){
+            ragdoll=!ragdoll;
+        }
+        if(ragdoll){
+            // childCollider.enabled = false;
+            animator.enabled=false;
+        }
+        else{
+           animator.enabled=true;
+           
+        }
         float h = Input.GetAxis("Horizontal");
         if(h > 0 && !facingRight)
             Flip();
